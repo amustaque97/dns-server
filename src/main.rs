e1373fe7d2e0a72ed96371ddf044d30c59663f2a
Mustaque Ahmed <amustaque97@gmail.com>
2023-06-24 20:43:03 +0530

    add stub server - complete chap 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::{fs::File, io::Read};
+use std::net::UdpSocket;
 use byte_packet_buffer::BytePacketBuffer;
 use dns_packet::DnsPacket;
+
+use crate::{query_type::QueryType, dns_question::DnsQuestion};
 
 mod byte_packet_buffer;
 mod result_code;
@@ -14,11 +16,40 @@ type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    let mut f = File::open("response_packet.txt")?;
-    let mut buffer = BytePacketBuffer::new();
-    f.read(&mut buffer.buf)?;
+    // Perform an A query for google.com
+    let qname = "google.com";
+    let qtype = QueryType::A;
 
-    let packet = DnsPacket::from_buffer(&mut buffer)?;
+    // Using google public DNS server
+    let server = ("8.8.8.8", 53);
+
+    // Bind a UDP socket to an arbitary port
+    let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
+
+    // Build our query packet. It's important that we remember to set the
+    // `recursion_desired` flag. As noted earlier, the packet ID is arbitrary.
+    let mut packet = DnsPacket::new();
+
+    packet.header.id = 6666;
+    packet.header.questions = 1;
+    packet.header.recursion_desired = true;
+    packet.questions.push(DnsQuestion::new(qname.to_string(), qtype));
+
+    // Use our new write method to write the packet to buffer...
+    let mut req_buffer = BytePacketBuffer::new();
+    packet.write(&mut req_buffer)?;
+
+    // and send it off to the server using our socket
+    socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
+
+    // To prepare for receiving the response, we'll create a new `BytePacketBuffer`,
+    // and ask the socket to write the response directly into our buffer.
+    let mut res_buffer = BytePacketBuffer::new();
+    socket.recv_from(&mut res_buffer.buf)?;
+
+    // As per the previous section, `DnsPacket::from_buffer()` is then used to 
+    // actually parse the packet after which we can print the response.
+    let packet = DnsPacket::from_buffer(&mut res_buffer)?;
     println!("{:#?}", packet.header);
 
     for q in packet.questions {
